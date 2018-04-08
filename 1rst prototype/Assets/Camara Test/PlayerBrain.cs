f887779c2dcb6d8a213da6c109abafa60116348b
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-04-08 19:14:40 -0300

    Level set
    blocking del nivel hecho, con pruebas de salto en altura, unas escaleras, salto en diagonal y salto en diagonal-altura.

---
+++ 1rst prototype/Assets/Camara Test/PlayerBrain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBrain : MonoBehaviour {
+    public bool ground;
+    public float speed 
+        , acceleration
+        , jumpForce
+        , currentSpeed
+        //No se que nombre ponerle esta variable, hace -speed o speed segun la orientacion
+        , playerSpeed;
+    void FixedUpdate () {
+        playerSpeed = Input.GetAxisRaw("Horizontal") * speed;
+
+    }
+}
