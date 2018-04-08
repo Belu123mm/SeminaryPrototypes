5408e6ad8285c79cf408cd271d0fadefd17f18ad
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-04-07 23:48:47 -0300

    BasePrototype

---
+++ 1rst prototype/Assets/Audio/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound testsound;
+
+    public static AudioManager instance;
+
+    void Awake() {
+        if ( instance != null ) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+        // Use this for initialization
+        void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1rst prototype/Assets/DungeonCharacters/AnimationController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+    public Animator anim;
+    public void Update() {
+        anim.SetBool("q", false);
+        anim.SetBool("w", false);
+        anim.SetBool("e", false);
+        anim.SetBool("r", false);
+
+
+        //anim.SetFloat("habx", Input.GetAxis("Horizontal"));
+        if ( Input.GetKey(KeyCode.Q) ) {
+            anim.SetBool("q", true);
+            return;
+            //anim.SetFloat("habx", Input.GetAxis("Q"));
+
+        }
+        
+        if ( Input.GetKeyDown(KeyCode.W) ) {
+            //anim.SetFloat("habx", Input.GetAxis("W"));
+            anim.SetBool("w", true);
+            return;
+
+
+        }
+        if ( Input.GetKeyDown(KeyCode.E) ) {
+            //anim.SetFloat("haby", Input.GetAxis("E"));
+            anim.SetBool("e", true);
+            return;
+
+        }
+        if ( Input.GetKeyDown(KeyCode.R) ) {
+            //anim.SetFloat("haby", Input.GetAxis("R"));
+            anim.SetBool("r", true);
+            return;
+
+        }
+        if ( Input.GetKey(KeyCode.T) ) {
+            anim.SetBool("FrontDashtg", true);
+            //anim.SetFloat("dashy", 1f);
+        }
+        if ( Input.GetKey(KeyCode.Y) ) {
+            anim.SetBool("FrontDashtg", false);
+        }
+    }
+
+}
+
+        
+    
