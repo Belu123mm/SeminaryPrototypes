2482f24855cef79d5e10a18dedeaa4c12571205f
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-04-12 00:41:23 -0300

    rehacion de las camaras

--- 1rst prototype/Assets/Camara Test/Gizmoz.cs
+++ 1rst prototype/Assets/Camara Test/Gizmoz.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gizmoz : MonoBehaviour {
+    public int xOffset, yOffset, zOffset;
     public void OnDrawGizmos() {
-        Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.forward * 5);     //tres horas haciendo esto, OFF
+        Gizmos.DrawLine(this.transform.position + new Vector3 (xOffset,yOffset,zOffset), this.transform.position + new Vector3(xOffset, yOffset, zOffset) + this.transform.forward * 5);     //tres horas haciendo esto, OFF
     }
 }
--- 1rst prototype/Assets/Camara Test/Movement.cs
+++ 1rst prototype/Assets/Camara Test/Movement.cs
@@ -6,36 +6,17 @@ using Cinemachine;
 public class Movement : MonoBehaviour {
     public float movementSpeed;
     public Rigidbody rb;
-    public Controller controller;
     public Camera cam;
-    public CinemachineBrain cmBrain;
-    public CinemachineFreeLook cmFreeLook;
     public void Start() {
         rb = GetComponent<Rigidbody>();
         cam = FindObjectOfType<Camera>();
-        cmBrain = cam.GetComponent<CinemachineBrain>();
-        cmFreeLook = cam.GetComponent<CinemachineFreeLook>();
     }
     public void Move( Vector3 direc ) {
-        cmBrain.enabled = false;
-        cmFreeLook.enabled = false;
+        rb.MovePosition(Vector3.Lerp(this.transform.position, this.transform.position + direc, 0.5f) * movementSpeed);
+        Quaternion rotation = Quaternion.LookRotation(direc);
+        rb.MoveRotation(rotation);
 
-        if ( controller == Controller.Tercera ) {
-
-            //Si ves a direcciones distintas, el lookrotation es cero. Hay que hacer un lerp
-            rb.MovePosition(Vector3.Lerp(this.transform.position, this.transform.position + direc, 0.5f) * movementSpeed);
-            Quaternion rotation = Quaternion.LookRotation(direc);
-            rb.MoveRotation(rotation);
-
-        }
-        if ( controller == Controller.Primera ) {
-            cmBrain.enabled = true;
-            cmFreeLook.enabled = true;
-        }
     }
+}
 
-}
-public enum Controller {
-    Tercera,
-    Primera
-}
+
