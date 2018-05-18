4014711b664c5e343b08511224ebb40e5cb79f12
GastonSantamaria <38821268+GastonSantamaria@users.noreply.github.com>
2018-05-18 01:56:52 -0300

    Add files via upload

--- 2nd prototype/Assets/Enemies/BearStates/BearState.cs
+++ 2nd prototype/Assets/Enemies/BearStates/BearState.cs
@@ -5,8 +5,24 @@ public class BearState : State
 {
     protected Bear myBear;
 
+    protected Movement _target;
+    protected float _speed;
+    protected float _chargeSpeed;
+    protected float _rotationSpeed;
+    protected float _timeOfPrediction;
+
+    protected Vector3 _predictedPosition = Vector3.zero;
+    protected Vector3 _dirToGo;
+
     public BearState(StateMachine sm, Bear b) : base(sm)
     {
         myBear = b;
+        _target = myBear.target;
+        _speed = myBear.speed;
+        _chargeSpeed = myBear.chargeSpeed;
+        _rotationSpeed = myBear.rotationSpeed;
+        _timeOfPrediction = myBear.timeOfPrediction;
+        _predictedPosition = myBear.predictedPosition;
+        _dirToGo = myBear.dirToGo;
     }
 }
