1bebd02dccc627fc9998381262883e15e96f7e72
GastonSantamaria <38821268+GastonSantamaria@users.noreply.github.com>
2018-05-28 19:51:59 -0300

    Decision Tree Update

---
+++ 2nd prototype/Assets/Enemies/BearStates/BearStateDie.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearStateDie : BearState
+{
+    public BearStateDie(StateMachine sm, Bear b) : base(sm, b)
+    {
+    }
+
+    public override void Awake()
+    {
+        Debug.Log("Entró a Die");
+        base.Awake();
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+    }
+
+    public override void Sleep()
+    {
+        Debug.Log("Salió de Die");
+        base.Sleep();
+    }
+}
